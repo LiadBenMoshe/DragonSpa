e75bcf4a1c1d0f0a1e56f4318d585a5081483142
liad <liadbenmoshe10@gmail.com>
2020-11-24 14:21:35 +0200

    user

---
+++ app/src/main/java/com/example/dragonspa/Client.java
@@ -0,0 +1,16 @@
+package com.example.dragonspa;
+
+public class Client {
+    public String name;
+    public String phoneNumber;
+
+    public Client()
+    {
+    }
+    public Client(String name, String phoneNumber)
+    {
+        this.name = name;
+        this.phoneNumber=phoneNumber;
+
+    }
+}
--- app/src/main/java/com/example/dragonspa/register_activity.java
+++ app/src/main/java/com/example/dragonspa/register_activity.java
@@ -14,15 +14,40 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class register_activity extends AppCompatActivity {
-    private EditText userName, userEmail, userPassword;
+    private EditText userName, userEmail, userPassword,userPhone;
     private Button register;
+    FirebaseDatabase mDatabase;
+    DatabaseReference dbRootRef;
+
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DatabaseReference.CompletionListener completionListener = new
+                DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference)
+                    {
+                        if (databaseError != null)
+                        {
+                            Toast.makeText(register_activity.this,databaseError.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(register_activity.this,"Saved!!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                };
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activity);
         setupUIviews();
@@ -37,10 +62,15 @@ public class register_activity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
-                                Toast.makeText(register_activity.this,"register secssesfull",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(register_activity.this,"register secssesfull",Toast.LENGTH_LONG).show();
+                                mDatabase = FirebaseDatabase.getInstance();
+                                dbRootRef = mDatabase.getReference();
+                                Client client = new Client(userName.getText().toString(),userPhone.getText().toString());
+                                client.phoneNumber = dbRootRef.push().getKey();
+                                dbRootRef.child("clients").child(userPhone.getText().toString()).setValue(client, completionListener);
                                 startActivity(new Intent(register_activity.this,MainActivity.class));
                             }else{
-                                Toast.makeText(register_activity.this,"register failed",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(register_activity.this,"register failed",Toast.LENGTH_LONG).show();
 
                             }
                         }
@@ -48,13 +78,17 @@ public class register_activity extends AppCompatActivity {
 
                 }
             }
+
+
         });
+
     }
 
     private void setupUIviews() {
         userName = (EditText) findViewById(R.id.edUserName);
         userEmail = (EditText) findViewById(R.id.edUserEmail);
         userPassword = (EditText) findViewById(R.id.edUserPassword);
+        userPhone=(EditText)findViewById(R.id.editTextPhone);
         register = (Button) findViewById(R.id.register);
     }
 
@@ -63,8 +97,9 @@ public class register_activity extends AppCompatActivity {
         String name = userName.getText().toString();
         String Email = userEmail.getText().toString();
         String Password = userPassword.getText().toString();
+        String Phone = userPhone.getText().toString();
 
-        if (name.isEmpty() || Email.isEmpty() || Password.isEmpty()) {
+        if (name.isEmpty() || Email.isEmpty() || Password.isEmpty() || Phone.isEmpty()) {
             Toast.makeText(this, "please enter all the details", Toast.LENGTH_LONG).show();
         } else {
             ans = true;
